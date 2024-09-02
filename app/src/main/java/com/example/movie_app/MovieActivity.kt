9ea1b930820e8708772992fe13e77ebe8e8f276c
nazliogel <ogelnazli@gmail.com>
2024-09-02 17:22:16 +0300

    added viewBinding for navigate to  DashBoardActivity

--- app/src/main/java/com/example/movie_app/MovieActivity.kt
+++ app/src/main/java/com/example/movie_app/MovieActivity.kt
@@ -1,11 +1,31 @@
 package com.example.movie_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movie_app.databinding.ActivityMovieBinding
 
 class MovieActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
+        binding = ActivityMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Butona tıklama olayını ayarla
+        setupButtonClickListener()
+    }
+
+    private fun setupButtonClickListener() {
+        binding.button.setOnClickListener {
+            navigateToDashboard()
+        }
+    }
+
+    private fun navigateToDashboard() {
+        val intent = Intent(this, DashBoardActivity::class.java)
+        startActivity(intent)
     }
 }
