c66f8fbe72574267ae58cf23a412ee9e930ed243
nazliogel <ogelnazli@gmail.com>
2024-09-02 16:42:03 +0300

    added movieActivity to the project

---
+++ app/src/main/java/com/example/movie_app/MovieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.movie_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
