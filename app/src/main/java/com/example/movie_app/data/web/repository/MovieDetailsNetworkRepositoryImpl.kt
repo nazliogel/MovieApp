3502edcb0fe3581c4d5df4776ce17cb954d4ab27
nazliogel <ogelnazli@gmail.com>
2024-09-05 14:58:09 +0300

    data/web paketleri ve alt paketleri olan model,repository projeye eklendi

---
+++ app/src/main/java/com/example/movie_app/data/web/repository/MovieDetailsNetworkRepositoryImpl.kt
@@ -0,0 +1,24 @@
+import com.example.movie_app.data.web.model.MovieDetailsResponse
+import com.example.movie_app.utils.Constant
+import com.example.movie_app.data.web.model.MovieListResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieServices {
+    @GET("movie/popular")
+    suspend fun getMovies(
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("api_key")
+        apiKey: String = Constant.API_KEY
+    ): MovieListResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id")
+        id: String,
+        @Query("api_key")
+        apiKey: String = Constant.API_KEY
+    ): MovieDetailsResponse
+}
--- app/src/main/java/com/example/movie_app/utils/Constant.kt
+++ app/src/main/java/com/example/movie_app/utils/Constant.kt
@@ -1,14 +1,12 @@
 package com.example.movie_app.utils
 
 object Constant {
-    object Constant {
-        const val API_KEY = "5876fce8b03c95e029be990a14710993"
-        const val READ_TIMEOUT = 300L
-        const val WRITE_TIMEOUT = 300L
-        const val READ_TIMEOUT_LONG = 600L
-        const val WRITE_TIMEOUT_LONG = 600L
+    const val API_KEY = "5876fce8b03c95e029be990a14710993"
+    const val READ_TIMEOUT = 300L
+    const val WRITE_TIMEOUT = 300L
+    const val READ_TIMEOUT_LONG = 600L
+    const val WRITE_TIMEOUT_LONG = 600L
 
-        //const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
-    }
+    // const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 }
